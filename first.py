4a70dde439da7060eab8aa66a7863672e30af0ad
yousefhaitham2 <youssef.haitham@meti.dev>
2024-12-30 06:48:29 -0800

    Add files via upload

---
+++ first.py
@@ -0,0 +1,11 @@
+def introduction():
+    name = "yousef haitham"
+    age = 22  # Although I am a model, let's assume I'm 3 years old for this example.
+    learning_goals = "I want to learn more about how humans interact with AI and how I can assist in different projects effectively."
+
+    print("Hello! My name is", name)
+    print("I am", age, "years old.")
+    print("In this project,", learning_goals)
+
+if __name__ == "__main__":
+    introduction()
